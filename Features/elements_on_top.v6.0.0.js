59cc6eae317e0ced9a010c34c923cdee9fc40861
Rafael <rafaelgs0496@gmail.com>
2017-05-01 18:28:19 -0300

    Update and rename elements_on_top.v1.js to elements_on_top.v6.0.0.js

--- Features/elements_on_top.v6.0.0.js
+++ Features/elements_on_top.v6.0.0.js
@@ -1,9 +1,9 @@
-function move_pagination_navigation_to_top() {
+function load_pagination_navigation_on_top() {
     esgst.pagination_navigation.classList.add(`page_heading_btn`);
-    esgst.main_heading.appendChild(esgst.pagination_navigation);
+    esgst.main_page_heading.appendChild(esgst.pagination_navigation);
 }
 
-function move_active_discussions_to_top() {
+function load_active_discussions_on_top() {
     esgst.active_discussions.classList.remove(`widget-container--margin-top`);
     esgst.active_discussions.classList.add(`eot_active_discussions`);
     var parent = esgst.active_discussions.parentElement;
