ef840b0177136f07102ff8037d53adcf2269a7a4
Rafael <rafaelgs0496@gmail.com>
2017-05-01 22:03:35 -0300

    Update embedded_videos.v6.0.0.js

--- Features/embedded_videos.v6.0.0.js
+++ Features/embedded_videos.v6.0.0.js
@@ -1,13 +1,13 @@
 function load_embedded_videos() {
     var matches, i, n;
-    matches = document.querySelectorAll(`a[href*=youtube.com]`);
+    matches = document.querySelectorAll(`a[href*="youtube.com"]`);
     for (i = 0, n = matches.length; i < n; ++i) {
         matches[i].outerHTML = `
             <iframe width="420" height="315"
             src="https://www.youtube.com/embed/${matches[i].getAttribute(`href`).match(/=(.+)/)[1]}"></iframe>
         `;
     }
-    matches = document.querySelectorAll(`a[href*=youtu.be]`);
+    matches = document.querySelectorAll(`a[href*="youtu.be"]`);
     for (i = 0, n = matches.length; i < n; ++i) {
         matches[i].outerHTML = `
             <iframe width="420" height="315"
