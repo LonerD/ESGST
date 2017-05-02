5e35dd0ed2c47de58fbf98f5fd1bcc4f259e5fe4
Rafael <rafaelgs0496@gmail.com>
2017-05-02 12:35:12 -0300

    Update and rename header_icons_refresher.v6.0.0 to header_icons_refresher.v6.0.0.js

--- Features/header_icons_refresher.v6.0.0.js
+++ Features/header_icons_refresher.v6.0.0.js
@@ -1,19 +1,22 @@
 function load_header_icons_refresher() {
+    var created_icon, won_icon, inbox_icon, last_count, refresher;
     created_icon = document.getElementsByClassName(`nav__right-container`)[0].firstElementChild;
     won_icon = created_icon.nextElementSibling;
     inbox_icon = won_icon.nextElementSibling;
-    load_won_page();
-    interal = setInterval(load_won_page, 60000);
     window.addEventListener(`focus`, load_won_page);
     if (esgst.hir_b) {
-        window.addEventListener(`blur`, stop_refresher);
+        window.addEventListener(`blur`, deactivate_header_icons_refresher);
     }
+    load_won_page();
     
-    function load_won_page() {    
+    function load_won_page() {
         makeRequest(null, `/giveaways/won`, null, refresh_icons);
+        refresher = setInterval(load_won_page, 60000);
     }
     
     function refresh_icons(response) {
+        var response_html, updated_created_icon, updated_won_icon, updated_inbox_icon,
+            matches, delivered, i, n, received, not_received, count;
         response_html = parseHTML(response.responseText);
         updated_created_icon = response_html.getElementsByClassName(`nav__right-container`)[0].firstElementChild;
         updated_won_icon = updated_created_icon.nextElementSibling;
@@ -22,19 +25,21 @@ function load_header_icons_refresher() {
         created_icon.innerHTML = updated_created_icon.innerHTML;
         won_icon.className = updated_won_icon.className;
         won_icon.innerHTML = updated_won_icon.innerHTML;
-        matches = response_html.getElementsByClassName("table__row-inner-wrap");
+        matches = response_html.getElementsByClassName(`table__row-inner-wrap`);
         delivered = 0;
         for (i = 0, n = matches.length; i < n; ++i) {
-            received = matches[i].getElementsByClassName("table__gift-feedback-received")[0];
-            not_received = matches[I].getElementsByClassName("table__gift-feedback-not-received")[0];
-            if ((received.classList.contains("is-hidden") && ((not_received && not_received.classList.contains("is-hidden")) || !not_received)) &&
-                matches[I].querySelector("[data-clipboard-text]")) {
+            received = matches[i].getElementsByClassName(`table__gift-feedback-received`)[0];
+            not_received = matches[I].getElementsByClassName(`table__gift-feedback-not-received`)[0];
+            if ((received.classList.contains(esgst.hidden_class) &&
+                ((not_received && not_received.classList.contains(esgst.hidden_class)) || !not_received)) &&
+                matches[I].querySelector("[data-clipboard-text]")
+            ) {
                 ++delivered;
             }
         }
         if (delivered > 0) {
-            won_icon.classList.add("DGNIcon");
-            won_icon.firstElementChild.title = "Giveaways Won (" + Delivered + " Delivered)";
+            won_icon.classList.add(`hir_won_icon_delivered`);
+            won_icon.firstElementChild.title = `Giveaways Won (${delivered} Delivered)`;
         }
         inbox_icon.className = updated_inbox_icon.className;
         inbox_icon.innerHTML = updated_inbox_icon.innerHTML;
@@ -46,15 +51,14 @@ function load_header_icons_refresher() {
                 if (count > 9) {
                     count = 0;
                 }
-                document.querySelector("link[rel='shortcut icon']").href = getResourceURL(`hir_icon_${count}`);
+                esgst.icon.href = getResourceURL(`hir_icon_${count}`);
             }
         } else {
-            count = 0;
-            document.querySelector("link[rel='shortcut icon']").href = getResourceURL(`steamgifts_icon`);
+            esgst.icon.href = getResourceURL(`steamgifts_icon`);
         }
     }
     
-    function stop_refresher() {
+    function deactivate_header_icons_refresher() {
         clearInterval(refresher);
     }
 }
