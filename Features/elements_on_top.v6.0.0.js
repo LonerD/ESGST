0b128a4995c500c69be76b114c408c413978dd6d
Rafael <rafaelgs0496@gmail.com>
2017-05-02 11:28:00 -0300

    Update elements_on_top.v6.0.0.js

--- Features/elements_on_top.v6.0.0.js
+++ Features/elements_on_top.v6.0.0.js
@@ -9,3 +9,13 @@ function load_active_discussions_on_top() {
     var parent = esgst.active_discussions.parentElement;
     parent.insertBefore(esgst.active_discussions, parent.firstElementChild);
 }
+
+function load_main_comment_box_on_top() {
+    var html, eot_main_comment_box;
+    html = `
+        <div class="eot_main_comment_box"></div>
+    `;
+    esgst.main_page_heading_background.insertAdjacentHTML(`afterEnd`, html);
+    eot_main_comment_box = esgst.main_page_heading_background.nextElementSibling;
+    eot_main_comment_box.appendChild(esgst.main_comment_box);
+}
