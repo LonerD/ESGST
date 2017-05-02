eeee66426d7c7ba8db1c13c9fc584d3bb32c7811
Rafael <rafaelgs0496@gmail.com>
2017-05-02 12:44:47 -0300

    Update header_icons_refresher.v6.0.0.js

--- Features/header_icons_refresher.v6.0.0.js
+++ Features/header_icons_refresher.v6.0.0.js
@@ -1,8 +1,5 @@
 function load_header_icons_refresher() {
-    var created_icon, won_icon, inbox_icon, last_count, refresher;
-    created_icon = document.getElementsByClassName(`nav__right-container`)[0].firstElementChild;
-    won_icon = created_icon.nextElementSibling;
-    inbox_icon = won_icon.nextElementSibling;
+    var refresher;
     window.addEventListener(`focus`, load_won_page);
     if (esgst.hir_b) {
         window.addEventListener(`blur`, deactivate_header_icons_refresher);
@@ -15,35 +12,22 @@ function load_header_icons_refresher() {
     }
     
     function refresh_icons(response) {
-        var response_html, updated_created_icon, updated_won_icon, updated_inbox_icon,
+        var response_html, created_icon, won_icon, inbox_icon,
             matches, delivered, i, n, received, not_received, count;
         response_html = parseHTML(response.responseText);
-        updated_created_icon = response_html.getElementsByClassName(`nav__right-container`)[0].firstElementChild;
-        updated_won_icon = updated_created_icon.nextElementSibling;
-        updated_inbox_icon = updated_won_icon.nextElementSibling;
-        created_icon.className = updated_created_icon.className;
-        created_icon.innerHTML = updated_created_icon.innerHTML;
-        won_icon.className = updated_won_icon.className;
-        won_icon.innerHTML = updated_won_icon.innerHTML;
-        matches = response_html.getElementsByClassName(`table__row-inner-wrap`);
-        delivered = 0;
-        for (i = 0, n = matches.length; i < n; ++i) {
-            received = matches[i].getElementsByClassName(`table__gift-feedback-received`)[0];
-            not_received = matches[I].getElementsByClassName(`table__gift-feedback-not-received`)[0];
-            if ((received.classList.contains(esgst.hidden_class) &&
-                ((not_received && not_received.classList.contains(esgst.hidden_class)) || !not_received)) &&
-                matches[I].querySelector("[data-clipboard-text]")
-            ) {
-                ++delivered;
-            }
+        created_icon = response_html.getElementsByClassName(`nav__right-container`)[0].firstElementChild;
+        won_icon = created_icon.nextElementSibling;
+        inbox_icon = won_icon.nextElementSibling;
+        esgst.created_icon.className = created_icon.className;
+        esgst.created_icon.innerHTML = created_icon.innerHTML;
+        esgst.won_icon.className = won_icon.className;
+        esgst.won_icon.innerHTML = won_icon.innerHTML;
+        if (esgst.dgn) {
+            check_delivered_gifts(response);
         }
-        if (delivered > 0) {
-            won_icon.classList.add(`hir_won_icon_delivered`);
-            won_icon.firstElementChild.title = `Giveaways Won (${delivered} Delivered)`;
-        }
-        inbox_icon.className = updated_inbox_icon.className;
-        inbox_icon.innerHTML = updated_inbox_icon.innerHTML;
-        count = inbox_icon.getElementsByClassName(`nav__notification`)[0];
+        esgst.inbox_icon.className = inbox_icon.className;
+        esgst.inbox_icon.innerHTML = inbox_icon.innerHTML;
+        count = esgst.inbox_icon.getElementsByClassName(`nav__notification`)[0];
         if (count) {
             count = parseInt(count.textContent);
             if (count != last_count) {
