951481ea7e446fedcba1a9f807d88e5b1d4e62f1
Rafael <faelgs15@gmail.com>
2017-08-24 09:54:36 -0300

    v6.Beta.31.4

--- ESGST.meta.js
+++ ESGST.meta.js
@@ -1,3 +1,3 @@
 // ==UserScript==
-// @version 6.Beta.31.3
+// @version 6.Beta.31.4
 // ==/UserScript==
--- ESGST.user.js
+++ ESGST.user.js
@@ -3,7 +3,7 @@
 // @namespace ESGST
 // @description Enhances SteamGifts and SteamTrades by adding some cool features to them.
 // @icon https://dl.dropboxusercontent.com/s/lr3t3bxrxfxylqe/esgstIcon.ico?raw=1
-// @version 6.Beta.31.3
+// @version 6.Beta.31.4
 // @author revilheart
 // @downloadURL https://github.com/revilheart/ESGST/raw/master/ESGST.user.js
 // @updateURL https://github.com/revilheart/ESGST/raw/master/ESGST.meta.js
@@ -8073,17 +8073,17 @@
                 }
                 if (typeof gf[minKey] !== `undefined`) {
                     if (name === `MinutesToEnd`) {
-                        filtered = minutes > gf[minKey] ? false : true;
+                        filtered = minutes >= gf[minKey] ? false : true;
                     } else {
-                        filtered = giveaway[key] > gf[minKey] ? false : true;
+                        filtered = giveaway[key] >= gf[minKey] ? false : true;
                     }
                 }
                 if (!filtered) {
                     if (typeof gf[maxKey] !== `undefined`) {
                         if (name === `MinutesToEnd`) {
-                            filtered = minutes < gf[maxKey] ? false : true;
+                            filtered = minutes <= gf[maxKey] ? false : true;
                         } else {
-                            filtered = giveaway[key] < gf[maxKey] ? false : true;
+                            filtered = giveaway[key] <= gf[maxKey] ? false : true;
                         }
                     }
                 }
@@ -32387,6 +32387,15 @@ ${avatar.outerHTML}
     function loadChangelog(version) {
         var changelog, current, html, i, index, n, popup;
         changelog = [
+            {
+                date: `August 24, 2017`,
+                version: `6.Beta.31.4`,
+                changelog: `
+                    <ul>
+                        <li>Fixed a bug in Giveaway Filters that was filtering exceptions higher or lower than, but not equal.</li>
+                    </ul>
+                `
+            },
             {
                 date: `August 23, 2017`,
                 version: `6.Beta.31.3`,
